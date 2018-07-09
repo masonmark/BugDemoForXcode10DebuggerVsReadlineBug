cbd062dfaaf5fd8c2c39442b14829e36cc201b90
Mason Mark <mason@soracom.jp>
2018-07-09 13:36:49 +0900

    Initial Commit

---
+++ BugDemoForXcode10DebuggerVsReadlineBug/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BugDemoForXcode10DebuggerVsReadlineBug
+//
+//  Created by mason on 2018/07/09.
+//  Copyright © 2018 MASON MARK (.COM). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
